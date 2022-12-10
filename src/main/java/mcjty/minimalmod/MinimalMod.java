3865674aa0021c22c2b4d20298178c4ddcddb388
McJty <mcjty1@gmail.com>
2022-12-10 15:43:37 +0100

    Minimal mod

---
+++ src/main/java/mcjty/minimalmod/MinimalMod.java
@@ -0,0 +1,11 @@
+package mcjty.minimalmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MinimalMod.MODID)
+public class MinimalMod {
+    public static final String MODID = "minimalmod";
+
+    public MinimalMod() {
+    }
+}
